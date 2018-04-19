60dbac0996d470dceb7703e00df76a96afb071ac
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2018-04-19 02:52:39 -0400

    Create d_find.py

---
+++ timisoaractf/Those_Are_Rookie_Numbers/d_find.py
@@ -0,0 +1,18 @@
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+a = 65537
+m = 58900433780152059829684181006276669632563849616305906563893514443102586211160
+
+print(modinv(a,m))
